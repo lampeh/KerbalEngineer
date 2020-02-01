f9ff05cb34c386658c334da4ddef745804261b52
Hauke Lampe <lampe@hauke-lampe.de>
2020-02-01 18:02:18 +0100

    hide readouts in HUDs only

--- KerbalEngineer/Flight/Readouts/Vessel/SuicideBurnDistance.cs
+++ KerbalEngineer/Flight/Readouts/Vessel/SuicideBurnDistance.cs
@@ -44,7 +44,7 @@ namespace KerbalEngineer.Flight.Readouts.Vessel
 
         public override void Draw(Unity.Flight.ISectionModule section)
         {
-            if (!SimulationProcessor.ShowDetails || !Surface.ImpactProcessor.ShowDetails || Surface.ImpactProcessor.SuicideDeltaV == 0) {
+            if (!SimulationProcessor.ShowDetails || !Surface.ImpactProcessor.ShowDetails || (section.IsHud && Surface.ImpactProcessor.SuicideDeltaV == 0)) {
                 return;
             }
 
